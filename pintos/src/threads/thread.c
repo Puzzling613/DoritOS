4889d4d02b12a4170b3931aa593ca3f1a91f7946
hsprt218 <hsprt218@postech.ac.kr>
2024-10-01 07:51:27 +0000

    implement thread_awake

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -24,6 +24,8 @@
    that are ready to run but not actually running. */
 static struct list ready_list;
 
+static struct list block_list;
+
 /* List of all processes.  Processes are added to this list
    when they are first scheduled and removed when they exit. */
 static struct list all_list;
@@ -91,6 +93,7 @@ thread_init (void)
 
   lock_init (&tid_lock);
   list_init (&ready_list);
+  list_init (&block_list);
   list_init (&all_list);
 
   /* Set up a thread structure for the running thread. */
@@ -582,3 +585,22 @@ allocate_tid (void)
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
+
+void thread_awake(int64_t curr_time){
+  enum intr_level old_level;
+  old_level = intr_disable();
+
+  struct list_elem *e = list_begin(&block_list);
+  struct list_elem *final = list_end(&block_list);
+
+  while (e != final){
+    struct thread *t = list_entry (e, struct thread, elem);
+
+    if (t->alarm_time <= curr_time){
+      e = list_remove(e);
+      thread_unblock(t);
+    }
+    else break;
+  }
+  intr_set_level(old_level);
+}
