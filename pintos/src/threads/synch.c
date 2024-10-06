721f83982c4a5d3cdcf1194704b2def6e1cfc719
hsprt218 <hsprt218@postech.ac.kr>
2024-10-06 12:35:47 +0000

    implement sema_compare

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      list_insert_ordered (&sema->waiters, &thread_current ()->elem, list_compare, 0);
       thread_block ();
     }
   sema->value--;
@@ -264,6 +264,19 @@ cond_init (struct condition *cond)
   list_init (&cond->waiters);
 }
 
+bool
+sema_compare(struct list_elem *a, struct list_elem *b, bool *aux){
+  sema_a = list_entry(a, struct semaphore_elem, elem);
+  sema_b = list_entry(b, struct semaphore_elem, elem);
+
+  priority_a = thread_get_priority(list_entry(a, struct semaphore_elem, elem)->thread);
+  priority_b = thread_get_priority(list_entry(b, struct semaphore_elem, elem)->thread);
+
+  aux = priority_a > priority_b;
+  
+  return aux;
+}
+
 /* Atomically releases LOCK and waits for COND to be signaled by
    some other piece of code.  After COND is signaled, LOCK is
    reacquired before returning.  LOCK must be held before calling
@@ -295,7 +308,7 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  list_insert_ordered (&cond->waiters, &waiter.elem, sema_compare, 0);
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
