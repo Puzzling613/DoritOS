5fd59e2b3401382c05f3f3d7d6ad88cb8c15efaf
Jo Minseok <minseok3075@gmail.com>
2024-10-07 12:08:27 +0000

    reimplement sema_up

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -129,10 +129,11 @@ sema_up (struct semaphore *sema)
 
   old_level = intr_disable ();
   if (!list_empty (&sema->waiters)) 
-    list_sort(&sema->waiters, sema_compare, 0);
+    list_sort(&sema->waiters, list_compare, 0);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
   sema->value++;
+  thread_yield_after_compare();
   intr_set_level (old_level);
 }
 
